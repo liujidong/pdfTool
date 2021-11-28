727fdd3a65d1b6f78944a2c074997347efca2c94
liujidong <wwwliujidong@163.com>
2021-11-28 10:45:52 +0800

    优化

--- src/main/java/pdfTool/PDFtoImage.java
+++ src/main/java/pdfTool/PDFtoImage.java
@@ -8,15 +8,21 @@ import javax.imageio.ImageIO;
 
 public class PDFtoImage {
     public static void main(String[] args) throws IOException {
+    	if(args==null || args.length==0) {
+    		System.out.print("PDFtoImage xxx.pdf");
+    		System.exit(-1);
+    	}
         //实例化PdfDocument类的对象，并加载测试文档
         PdfDocument doc = new PdfDocument();
-        doc.loadFromFile("系统测试.pdf");
+        String filePath = args[0];
+        doc.loadFromFile(filePath);
 
         //遍历文档，并将文档保存为png格式的图片
         BufferedImage image;
+        String filePathName = filePath.substring(0,filePath.lastIndexOf("."));
         for (int i = 0; i < doc.getPages().getCount(); i++) {
             image = doc.saveAsImage(i);
-            File file = new File( String.format("ToImage-img-%d.png", i));
+            File file = new File( String.format(filePathName+"-img-%d.png", i));
             ImageIO.write(image, "PNG", file);
         }
 
